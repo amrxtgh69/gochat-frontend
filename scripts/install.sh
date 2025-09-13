8321509885b119ffb03374c786b4c555d7d101a6
Rajesh Thapa <devrajeshthapa@gmail.com>
2025-09-13 23:08:35 +0545

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,1 +1,20 @@
-# script to install GoChat in Linux/MacOS
+#!/bin/bash
+set -e  # Exit immediately if a command exits with a non-zero status
+
+# Variables
+REPO_URL="https://github.com/amrxtgh69/gochat-frontend"
+TMP_DIR="/tmp/gochat_clone"
+OUTPUT_BIN="/usr/local/bin/gochat"
+
+# Clone the repository
+git clone "$REPO_URL" "$TMP_DIR"
+
+# Build the Go project
+cd "$TMP_DIR"
+go build -o "$OUTPUT_BIN" cmd/gochat/main.go
+
+# Clean up
+cd /
+rm -rf "$TMP_DIR"
+
+echo "GoChat built and installed at $OUTPUT_BIN"
